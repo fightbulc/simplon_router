0b61de3df776fa31cd3d2ad067e2bf8c43acb853
Tino Ehrich <ehrich@efides.com>
2014-11-12 22:55:02 +0100

    udpated readme and test

--- test/test.php
+++ test/test.php
@@ -5,18 +5,22 @@ require __DIR__ . '/TestController.php';
 
 $routes = [
     [
+        'request'  => 'GET',
         'pattern'  => '^hello$',
         'callback' => function () { return 'How kind!'; },
     ],
     [
+        'request'  => 'GET',
         'pattern'  => 'hello/(\w+)$',
         'callback' => function ($param) { return 'How kind, ' . $param; },
     ],
     [
+        'request'    => 'POST',
         'pattern'    => 'foo/(\w+)$',
         'controller' => 'TestController::fooAction',
     ],
     [
+        'request'  => 'POST',
         'pattern'  => '.',
         'callback' => function () { return '404'; },
     ],
